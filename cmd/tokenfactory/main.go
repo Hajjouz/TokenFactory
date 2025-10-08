2f39fdd80ad0a6586322e651de97b69fcf0eb5b3
Hajjouz <18026754+Hajjouz@users.noreply.github.com>
2025-10-08 12:18:38 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tokenfactory/main.go
@@ -0,0 +1,23 @@
+// cmd/tokenfactory/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tokenfactory/internal/tokenfactory"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tokenfactory.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
